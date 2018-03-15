13166ed34ab5cda918679e9be9b2f2678a5a7821
Holger Mueller <9jholger@gmail.com>
2018-03-15 14:02:02 -0500

    a little css and pseudo code

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -4,6 +4,19 @@ const quizBody = document.getElementById('quiz');
 const resultHolder = document.getElementById('results');
 const submitButton = document.getElementById('results');
 
+//button to begin quiz
+
+//set up timer
+
+//questions
+
 const myQuestions = [
 
 ]
+
+//submit button
+
+//function to determine correct answers
+
+//display results
+
