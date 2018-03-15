b5fb26caf3de2e9b4918f2e0ade2017a4507d635
Holger Mueller <9jholger@gmail.com>
2018-03-14 22:34:43 -0500

    redo html

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const quizBody = document.getElementById('quiz');
+const resultHolder = document.getElementById('results');
+const submitButton = document.getElementById('results');
+
+const myQuestions = [
+
+]
