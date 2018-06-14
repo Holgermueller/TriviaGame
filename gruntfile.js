ceea5bf7128e3124f5948933bede6947a93623ad
Holger Mueller <9jholger@gmail.com>
2018-06-13 22:18:28 -0500

    got less and grunt running

---
+++ gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    './assets/css/style.css': './assets/css/style.less'
+                }
+            }
+        }, watch: {
+            files: ['assets/**/*.less'],
+            tasks: ['less'],
+            options: {
+                nospawn: true,
+                livereload: true
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['less', 'watch']);
+};
